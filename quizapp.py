0de0a0745f1bca839af6a0e3023d35fc9977afcd
kumar adhikari <kumaradhikari7@gmail.com>
2022-01-06 22:59:19 -0600

    second commit

--- quizapp.py
+++ quizapp.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-
+questions = {"What is capital of nepal?": "kathmandu", "What is capital of india?": "new delhi",
+             "How many meter in kilometer?": "1000"}
 @app.route("/")
 def home():
-    return render_template("index.html", content="kumar")
+    return render_template("index.html", content=questions)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
